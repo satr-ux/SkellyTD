8a353ac625dbec9b7cdc8f753d152310e0820db4
satr-ux <241788167+satr-ux@users.noreply.github.com>
2026-05-10 14:50:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-ghd
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve files from /public
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+  console.log(`Tower Defense running on http://localhost:${PORT}`);
+});
